bdf8eb4837e407357977331422dfa525bd6f5ded
Emrecan Ay <codemrecan@gmail.com>
2022-11-03 11:46:09 +0300

    initial commit

---
+++ src/test/java/kodlamaio/programmingLanguages/ProgrammingLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlamaio.programmingLanguages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
